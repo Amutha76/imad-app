0231da7092fc2337c8cf9606ab89259830d09da9
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-14 15:45:48 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,2 @@
 console.log('Loaded!');
 
-// Changing inner html value
-var element=document.getElementById("main-id");
-
-alert("Hi, I am Javascript");
-
-element.innerHTML= "New Value";
-
-
-//Moving the image
-var madi=document.getElementById("madi");
-var marginleft=0;
-
-function moveright(){
-  marginleft=marginleft+10;
-  madi.style.marginLeft=marginleft +'px';
-}
-
-madi.onclick=function(){
-   
-var interval=setInterval(moveright,100);
-};
-  
