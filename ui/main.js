3bb1ebbceb67ce169c765e9b156819917f2c46e4
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-14 14:36:12 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,13 @@
 console.log('Loaded!');
 
-
+// Changing inner html value
 var element=document.getElementById("main-id");
 
 alert("Hi, I am Javascript");
 
 element.innerHTML= "New Value";
+
+
+//Moving the image
 
   
