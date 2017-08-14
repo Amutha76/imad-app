24a4ea778a15c93acaeca0c0914905bac3b3524e
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-14 13:29:28 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/:articleName', function(req,res){
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles(articleName)));
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two', function(req,res){
