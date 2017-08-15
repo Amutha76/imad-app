7d441b001beeb446937776057b6fbe6cdbfa5911
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-15 18:21:45 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 var button=document.getElementById('btncounter');
-alert('I am in main.js');
+console.log('main.js');
 var counter=0;
 button.onclick=function(){
-    alert('I am inside onclick in main.js');
+    
     counter=counter+1;
     var span=document.getElementById('spncount');
     span.innerHTML=counter.toString();
