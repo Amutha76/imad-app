2df5cad9762df00dff8d3c8b067c43ca728f20ac
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-15 17:54:57 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,9 @@
-console.log('Loaded!');
+var button=document.getElementById('btncounter');
+var counter=0;
+button.onClick=function(){
+    
+    counter=counter+1;
+    var span=document.getElementById('spncount');
+    span.innerHTML=counter.toString();
+};
 
