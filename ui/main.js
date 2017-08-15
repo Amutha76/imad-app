acf9dd5ceb3f4bb7d768fff8c1681361fc5ad563
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-15 18:01:23 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,6 @@ var button=document.getElementById('btncounter');
 
 var counter=0;
 button.onclick=function(){
-    alert(' I am inside click function');
     counter=counter+1;
     var span=document.getElementById('spncount');
     span.innerHTML=counter.toString();
