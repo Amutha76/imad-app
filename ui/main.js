b855da2b66e2bced8de8c59da2a92bacc6fb7e93
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-15 19:46:08 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,13 +4,15 @@ button.onclick=function(){
     
     var request=new XMLHttpRequest();
      
-   
+    var counter=request.responseText;
+    
     alert('tes3t ' + request.readyState + ' ' + XMLHttpRequest.DONE);
+    alert('counter value' + counter);
     if (request.readyState=== 0){
          alert('test1 ' + request.status);
         if(request.status==200){
             alert('test');
-            var counter=request.responseText;
+          
             var span=document.getElementById('spncount');
             span.innerHTML=counter.toString();        
         }
