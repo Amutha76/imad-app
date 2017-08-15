27c71f773fc3e5d0c369c4040ff39ee01c014c63
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-15 17:44:44 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var button=getElementById('btncounter');
+var button=document.getElementById('btncounter');
 var counter=0;
 button.onClick=function(){
     counter=counter + 1;
-    var span=getElementById('spncount');
+    var span=document.getElementById('spncount');
     span.innerHTML=counter.toString();
 }
