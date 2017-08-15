77a7755af9b1c5f9844f6b211e66ade677bd4303
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-15 19:57:32 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,23 +3,22 @@ var button=document.getElementById('btncounter');
 button.onclick=function(){
     
     var request=new XMLHttpRequest();
-    request.open('GET','http://bamutha76.imad.hasura-app.io/counter',true);
-    request.send(null);    
-    var counter=request.responseText;
+  
+   
     
-    alert('tes3t ' + request.readyState + ' ' + XMLHttpRequest.DONE);
-    alert('counter value' + counter);
-    if (request.readyState=== 0){
-         alert('test1 ' + request.status);
+    request.onreadystatechange=function(){
+    if (request.readyState=== XMLHttpRequest.DONE){
+        
         if(request.status==200){
             alert('test');
-          
+            var counter=request.responseText;
             var span=document.getElementById('spncount');
             span.innerHTML=counter.toString();        
         }
     }
     
-    
+     request.open('GET','http://bamutha76.imad.hasura-app.io/counter',true);
+    request.send(null);     
     
 };
-
+};
