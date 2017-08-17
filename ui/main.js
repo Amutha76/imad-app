1f752f618e2acccea243df82dc64af73524d1b4e
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-17 16:15:16 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,32 +2,28 @@ var button=document.getElementById('btncounter');
 
 button.onclick=function(){
      var request=new XMLHttpRequest();
-     //request.open();
-     //request.send();
-     //method="GET",
-     //url="http://bamutha76.imad.hasura-app.io";
-     //request.open(method,url,true);
-     
-     //request.onreadystatechange=function(){
-       // alert('test 2');
-        //if (request.readyState=== XMLHttpRequest.DONE){
-            
-         //   if(request.status==200){
-          //      alert('test');
-        //        var counter=request.responseText;
-        //        var span=document.getElementById('spncount');
-        //        span.innerHTML=counter.toString();        
-        //    }
-    //    }
-    
      request.open('GET','http://bamutha76.imad.hasura-app.io/counter',true);
-     request.send(null);     
+     request.send(null);    
+     
+     request.onreadystatechange=function(){
+      alert('test 2');
+     if (request.readyState=== XMLHttpRequest.DONE){
+            
+            if(request.status==200){
+               alert('test');
+                var counter=request.responseText;
+                var span=document.getElementById('spncount');
+                span.innerHTML=counter.toString();        
+            }
+        }
     
-//};
+     
+    
+};
 
  
-request.onreadystatechange=function()
-{
-    alert('fff');
-};
+//request.onreadystatechange=function()
+//{
+  //  alert('fff');
+//};
 };
