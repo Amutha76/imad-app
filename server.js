0517face593b419bcd79238119a5c3fe50bee021
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-17 18:17:32 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ return htmlContent;
 }    
 
 var names=[];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) {
   var name=req.query.name;
   names.push(name);
   res.send(JSON.stringify(names));
