5704d722611040ef5e10c07ed81f85e115a4ec97
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-17 20:48:42 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,10 +94,7 @@ app.get('/article-one/submit-name', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function (req, res) {
- console.log('sds');
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
- });
+
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
@@ -108,10 +105,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-//app.get('/:articleName', function(req,res){
-//    var articleName = req.params.articleName;
-//    res.send(createTemplate(articles[articleName]));
-//});
+app.get('/:articleName', function(req,res){
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
