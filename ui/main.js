130cb19f434968541a8445008d23f4ea8c0687f5
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-17 16:02:33 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,35 @@
 var button=document.getElementById('btncounter');
 
 button.onclick=function(){
-     var request=new XMLHttpRequest();
+    // var request=new XMLHttpRequest();
      //request.open();
      //request.send();
      //method="GET",
      //url="http://bamutha76.imad.hasura-app.io";
      //request.open(method,url,true);
-     alert('kk'+request.onreadystatechange);
-     alert('readystate is ' + request.readyState);
-     request.onreadystatechange=function(){
-        alert('test 2');
-        if (request.readyState=== XMLHttpRequest.DONE){
+     
+     //request.onreadystatechange=function(){
+       // alert('test 2');
+        //if (request.readyState=== XMLHttpRequest.DONE){
             
-            if(request.status==200){
-                alert('test');
-                var counter=request.responseText;
-                var span=document.getElementById('spncount');
-                span.innerHTML=counter.toString();        
-            }
-        }
+         //   if(request.status==200){
+          //      alert('test');
+        //        var counter=request.responseText;
+        //        var span=document.getElementById('spncount');
+        //        span.innerHTML=counter.toString();        
+        //    }
+    //    }
     
-     request.open('GET','http://bamutha76.imad.hasura-app.io/counter',true);
-    request.send(null);     
+    // request.open('GET','http://bamutha76.imad.hasura-app.io/counter',true);
+    //request.send(null);     
     
-};
+//};
+
+if (window.XMLHttpRequest){
+    alert('a');
+}
+else
+{
+    alert('b');
+}
 };
