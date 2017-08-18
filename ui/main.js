16d66887846b1d32b641716043f7cb9533ebebec
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-18 06:02:49 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,36 @@ var submitName=document.getElementById("btnsubmitName");
 var articleOneSubmitName=document.getElementById("articleOnebtnSubmit");
 alert(articleOneSubmitName.value);
 
+articleOneSubmitName.onclick=function(){
+    alert('I am inside articleone submit');
+  var articleOneEleName=document.getElementById("articleOnetxtComment");  
+  var name=articleOneEleName.value;
+  var request=new XMLHttpRequest();
+  request.open('GET','http://bamutha76.imad.hasura-app.io/article-one/submit-name?name='+ name,true);
+  request.send(null); 
+ 
+  request.onreadystatechange=function(){
+  
+     if (request.readyState=== XMLHttpRequest.DONE){
+            
+            if(request.status==200){
+                var names=request.responseText;
+                var ulName=document.getElementById("articleOneUl");
+                names=JSON.parse(names);
+                 var list='';
+                for( var i=0; i<names.length ; i++){
+                    list+='<li>' + names[i] + '</li>';
+      
+                }
+                ulName.innerHTML=list;
+            }
+        }
+};
+ 
+
+};
+
+
 
 button.onclick=function(){
      var request=new XMLHttpRequest();
@@ -53,33 +83,4 @@ submitName.onclick=function(){
 };
 
 
-
-articleOneSubmitName.onclick=function(){
-    alert('I am inside articleone submit');
-  var articleOneEleName=document.getElementById("articleOnetxtComment");  
-  var name=articleOneEleName.value;
-  var request=new XMLHttpRequest();
-  request.open('GET','http://bamutha76.imad.hasura-app.io/article-one/submit-name?name='+ name,true);
-  request.send(null); 
- 
-  request.onreadystatechange=function(){
-  
-     if (request.readyState=== XMLHttpRequest.DONE){
-            
-            if(request.status==200){
-                var names=request.responseText;
-                var ulName=document.getElementById("articleOneUl");
-                names=JSON.parse(names);
-                 var list='';
-                for( var i=0; i<names.length ; i++){
-                    list+='<li>' + names[i] + '</li>';
-      
-                }
-                ulName.innerHTML=list;
-            }
-        }
-};
- 
-
-};
 
