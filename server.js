6b5e304844e221d9cefd4b0394b5ab912fbbd767
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-22 13:27:27 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/test-db', function(req,res){
         if (err){
             res.status(500).send(err.toString());
         }else{
-            res.send(JSON.stingify(result));
+            res.send(JSON.stringify(result));
         }
     });
 });
