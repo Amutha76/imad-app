eea596aa2cf89f69b363b67dbc157472e1977a88
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-22 17:58:48 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(morgan('combined'));
 console.log('server.js');
 
 function hash(input,salt){
+    console.log('I am inside hash');
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
 }
