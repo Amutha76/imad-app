0294b25eb5f4b101546fbb01c5043742a13d01fd
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-22 14:44:29 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/articles/:articleName', function(req,res){
     var articleName = req.params.articleName;
-    pool.query("select * from article where title='"+ articleName+ "'", function(err,result){
+    pool.query("select * from article where title= $1 ", articleName , function(err,result){
         if(err){
            res.status(500).send(err.toString());     
         }else if(result.rows.length===0){
