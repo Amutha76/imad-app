b274661c21b6030be4f0264c29a4324510e517da
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-22 18:06:18 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ function hash(input,salt){
 
 app.get('/hash/:input',function(req,res){
     console.log('I am about to call hash');
-    var hashedString=hash(req.params.input,'this-is-some-randome-string');
+    var hashedString=hash(req.params.input,'this is a new string');
     res.send(hashedString);
 });
 
