5970ff85e55fb3f160116fe1fa2b63cd1969166c
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-23 20:54:08 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ submitLogin.onclick=function(){
   var password=document.getElementById("pwdPassword").value;
  
   var request=new XMLHttpRequest();
-  request.open('POST','http://bamutha76.imad.hasura-app.io/login',true);
+  request.open('POST','/login',true);
   request.setRequestHeader('Content-Type','application/json');
   request.send(JSON.stringify({username:username,password:password})); 
  
