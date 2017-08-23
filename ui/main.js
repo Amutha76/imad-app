716848863e1dcd38dfcd8f83f2238a8b00c06afb
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-23 06:57:33 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,24 +9,21 @@ submitName.onclick=function(){
   var password=document.getElementById("pwdPassword").value;
  
   var request=new XMLHttpRequest();
-  request.open('GET','http://bamutha76.imad.hasura-app.io/submit-name?name='+ name,true);
-  request.send(null); 
+  request.open('POST','http://bamutha76.imad.hasura-app.io/login',true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({username:username,password:password})); 
  
   request.onreadystatechange=function(){
   
      if (request.readyState=== XMLHttpRequest.DONE){
             
             if(request.status==200){
-                var names=request.responseText;
-                var ulName=document.getElementById("ulName");
-                names=JSON.parse(names);
-                 var list='';
-                for( var i=0; i<names.length ; i++){
-                    list+='<li>' + names[i] + '</li>';
-      
-                }
-                ulName.innerHTML=list;
-            }
+               alert('User credentials are correct');
+             } else if (request.status==403){
+                 alert('Invalid username and password');
+             } else if (request.status==500){
+                 alert('Internal server error');
+             }
         }
     };
 
