ece689511e85bd0653ab54ce71b07372006f6301
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-23 16:03:02 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,5 +30,30 @@ submitLogin.onclick=function(){
 
 };
 
+submitLogin.onclick=function(){
+  
+  var username=document.getElementById("txtName").value;  
+  var password=document.getElementById("pwdPassword").value;
+ 
+  var request=new XMLHttpRequest();
+  request.open('POST','http://bamutha76.imad.hasura-app.io/create-user',true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({username:username,password:password})); 
+ 
+  request.onreadystatechange=function(){
+  
+     if (request.readyState=== XMLHttpRequest.DONE){
+            
+            if(request.status==200){
+               alert('User successfully created');
+             } else if (request.status==403){
+                 alert('Cannot create user');
+             } else if (request.status==500){
+                 alert('Internal server error');
+             }
+        }
+    };
+
+};
 
 
