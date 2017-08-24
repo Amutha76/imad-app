bba21c265eef0445b818900d40a895fea8b0f15c
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-24 14:32:40 +0100

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -3,6 +3,17 @@ var currentArticleTitle = window.location.pathname.split('/')[2];
 function onLoadcommentForm()
 {
     
+    var request=new XMLHttpRequest();
+    request.open('GET','/article-list',true);
+    request.send(null); 
 };
+
+function getcomments()
+{
+    var request=new XMLHttpRequest();
+    request.open('POST','/getcomments/'+currentArticleTitle,true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({articletitle:currentArticleTitle}));
+}
 
 onlLoadForm();
