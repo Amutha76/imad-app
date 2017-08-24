a7db4713ea2ee69ae5d41dd8e56f6def1d755846
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-24 18:21:41 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var ArticleTitle = window.location.pathname.split('/');
 if (ArticleTitle.length>1){
     getcomments();
 }else {
-    var currentArticleTitle = window.location.pathname.split('/')[2]
+    var currentArticleTitle = window.location.pathname.split('/')[2];
     getArticles();
 }
 
@@ -15,6 +15,30 @@ var entercomments = document.getElementById('entercomment');
 
 var submitLogin=document.getElementById("btnsubmitLogin");
 var submitRegister=document.getElementById("btnsubmitRegister");
+
+function getcomments()
+{
+    var request=new XMLHttpRequest();
+    request.open('POST','/getcomments/'+currentArticleTitle,true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({articletitle:currentArticleTitle}));
+    
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var commentsData = JSON.parse(this.responseText);
+                var commentshtml='';
+                for (i=0;i<commentsData.length;i++){
+                    commentsData+='<div>'+commentsData[i].comment+'</div><div>'+commentsData[i].date+'</div><p>';
+                }
+                displaycomments.innerHTML=commentsData;
+            }else{
+              displaycomments.innerHTML='No comments for this article';    
+            }
+        }
+         
+     };
+}
 
 function getArticles(){
     var articleList=document.getElementById("articles");
