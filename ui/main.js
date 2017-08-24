51f3bc309994f47f2fbb8c9360bc7741d1b79a48
Amutha76 <30666161+Amutha76@users.noreply.github.com>
2017-08-24 18:45:58 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 
 console.log("I am inside main");
-//var currentArticleTitle = window.location.pathname.split('/')[2];
+var currentArticleTitle = window.location.pathname.split('/')[2];
 var ArticleTitle = window.location.pathname.split('/');
 alert(ArticleTitle.length + window.location.pathname);
 if (ArticleTitle.length>2){
